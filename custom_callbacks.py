39f6731bd98e49331dcd12bf19cc64e057e947c2
gfluz94 <52358345+gfluz94@users.noreply.github.com>
2020-12-13 11:52:21 -0300

    adding custom callbacks module

---
+++ custom_callbacks.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
+import io
+from PIL import Image
+from IPython.display import Image as IPyImage
+import imageio
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import Callback
+
+GIF_PATH = './animation.gif'
+
+
+class VisCallback(Callback):
+    def __init__(self, inputs, ground_truth, display_freq=10, n_samples=10):
+        self.inputs = inputs
+        self.ground_truth = ground_truth
+        self.images = []
+        self.display_freq = display_freq
+        self.n_samples = n_samples
+
+    def on_epoch_end(self, epoch, logs=None):
+        indexes = np.random.choice(len(self.inputs), size=self.n_samples)
+        X_test, y_test = self.inputs[indexes], self.ground_truth[indexes]
+        true_labels = np.argmax(y_test, axis=1)
+        predictions = np.argmax(self.model.predict(X_test), axis=1)
+
+        display_digits(X_test, predictions, y_test, epoch, n=self.display_freq)
+
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
+        image = Image.open(buf)
+        self.images.append(np.array(image))
+
+        if epoch % self.display_freq == 0:
+            plt.show()
+
+    def on_train_end(self, logs=None):
+        imageio.mimsave(GIF_PATH, self.images, fps=1)
+
+
+plt.rc('font', size=20)
+plt.rc('figure', figsize=(15, 3))
+
+def display_digits(inputs, outputs, ground_truth, epoch, n=10):
+    plt.clf()
+    plt.yticks([])
+    plt.grid(None)
+    inputs = np.reshape(inputs, [n, 28, 28])
+    inputs = np.swapaxes(inputs, 0, 1)
+    inputs = np.reshape(inputs, [28, 28*n])
+    plt.imshow(inputs)
+    plt.xticks([28*x+14 for x in range(n)], outputs)
+    for i,t in enumerate(plt.gca().xaxis.get_ticklabels()):
+        if outputs[i] == ground_truth[i]: 
+            t.set_color('green') 
+        else: 
+            t.set_color('red')
+    plt.grid(None)
